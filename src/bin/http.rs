2db668c48a86142a3bd099ddc4d2cec8aa778267
jvcByte <jvc8463@gmail.com>
2026-07-22 14:14:08 +0100

    Fix: update response from Document/json to application/json

--- src/bin/http.rs
+++ src/bin/http.rs
@@ -7,12 +7,12 @@ fn main() {
         let mut req = req.unwrap();
         let req_addr = req.peer_addr().unwrap();
 
-        println!("Req: {:#?}\nAddr: {:#?}", req, req_addr);
+        println!("Req: {:?}\nAddr: {:?}", req, req_addr);
 
         let body = format!(r#"{{"Protocol": "HTTP/1.1","Your Addres is": "{req_addr}"}}"#);
 
         let response = format!(
-            "HTTP/1.1 200 Ok\r\nContent-Length: {}\r\nContent-Type: Document/json\r\nConnection: close\r\n\r\n{}",
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n{}",
             body.len(),
             body
         );
